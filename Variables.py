b0ad12812a2ff32c9b62da587b92d1bac71c0bca
Lewis Mansi <lewis.mansi@gmail.com>
2024-11-28 14:26:16 +0000

    Covering Variables

---
+++ Variables.py
@@ -0,0 +1,9 @@
+#Assigning variables to variables
+guest_count = 15
+event_location = "Central Park"
+is_catered = "True"
+
+#Updating the 'Guest Count' Variable by adding 3 additional guests
+guest_count = += 3
+
+print(f"Updated guest count: {guest_count} ")
